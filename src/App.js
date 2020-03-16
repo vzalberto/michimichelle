ab7dd9b298f0f376b55b79ff6f0227f12634728b
Alberto Zavala <alberto@getclue.com>
2020-03-15 19:09:34 -0600

    Contemplatoria

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useRef} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            lluvia : false
+        }
+    }
 
-export default App;
+    yaLoVes = () => {
+        return (
+            <div>
+                <p id="lalluvia">🌧🌧🌧🌧🌧🌧🌧</p>
+                <img id="michelle" src="/bajoLaLluvia.gif" onClick={this.miau} />
+            </div>
+        )
+    }
+
+    laVidaEsAsi = () => {
+        return (
+            <p 
+                className="emoji" 
+                onClick={this.miau}>
+                    <span 
+                    role='img'
+                        aria-label='cloud emoji nube'>
+                            ☁️
+                    </span>
+            </p>
+        )
+    }
+
+    miau = () => {
+        if (this.state.lluvia) {
+            this.rocioDurcal.pause();
+            this.rocioDurcal.currentTime = 0;
+        }
+        else {
+            this.rocioDurcal.play()
+        }
+        this.setState({lluvia: !this.state.lluvia})
+    }
+    
+    render () {
+        return (
+            <div className="App">
+                <header className="App-header">
+
+                    {
+                        this.state.lluvia ? 
+                            this.yaLoVes()
+                            : 
+                            this.laVidaEsAsi()       
+                    }
+                   
+                </header>
+
+                <audio ref={ref => {this.rocioDurcal = ref}} src="./gata_lluvia.mp3" />
+            </div>
+        );
+    }
+}
